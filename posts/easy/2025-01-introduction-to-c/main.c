81491cc688ae79cf9c5fdedca812bbb6f9b03a16
SystemProgrammerWizzard <tatandat110105@gmail.com>
2025-05-21 11:20:44 -0400

    [Main] : Set up the first posts
    Signed-off-by: SystemProgrammerWizzard <tatandat110105@gmail.com>

---
+++ posts/easy/2025-01-introduction-to-c/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // This is a processing directive that tells the compiler to include the standard library (printf function)
+
+int main() { // In C, the main function is the entry point of the program. There should be only 1 main inside the C project/
+    printf("Hello, World!\n"); // Print a string to the console. The \n is a special character that represents a new line.
+    return 0; // The return statement ends the main function and returns a value to the operating system. 0 usually indicates that the program ended successfully.
+}
+
